7fdf9d63599a1e93ed7c72054b170ccc689c7962
PuaChris <chris.pua@mail.utoronto.ca>
2020-06-03 16:56:27 -0400

    Removed tracking of dist folder

--- dist/devtools.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-chrome.devtools.panels.create(
-    "Domments",
-    "logo192.png",
-    "index.html",
-    function() {
-        console.log('Creating Domments panel.');
-    }
-);
